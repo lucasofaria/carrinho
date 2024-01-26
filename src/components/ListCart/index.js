7074032db3ee1b39c0d4996ec51d2f5a315bc453
lucasofaria <lucas092007@hotmail.com>
2024-01-26 00:12:20 -0300

    Style da ListCart OK

--- src/components/ListCart/index.js
+++ src/components/ListCart/index.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
 export default function ListCart({ data }) {
   const [ amount, setAmount ] = useState(data?.amount)
@@ -7,8 +7,20 @@ export default function ListCart({ data }) {
   return (
     <View style={styles.container}>
       <View>
-        <Text>{data.name}</Text>
-        <Text>{data.price}</Text>
+        <Text style={styles.item}>{data.name}</Text>
+        <Text style={styles.price}>R$ {data.price}</Text>
+      </View>
+
+      <View style={styles.areaButtons}>
+        <TouchableOpacity style={styles.buttonAdd}>
+          <Text style={styles.textButton}>+</Text>
+        </TouchableOpacity>
+
+        <Text style={styles.amount}>{amount}</Text>
+
+        <TouchableOpacity style={styles.buttonAdd}>
+          <Text style={styles.textButton}>-</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -16,6 +28,45 @@ export default function ListCart({ data }) {
 
 const styles = StyleSheet.create({
   container:{
-    
+    borderWidth: 1,
+    backgroundColor: '#FFF',
+    borderRadius: 6,
+    marginBottom: 14,
+    padding: 10
+  },
+
+  item:{
+    fontSize: 18,
+    fontWeight: 'bold'
+  },
+
+  price:{
+    fontSize: 16
+  },
+
+  areaButtons:{
+    flexDirection: 'row',
+    marginTop: 10,
+    alignItems: 'center'
+  },
+
+  amount:{
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginLeft: 10,
+    marginRight: 10,
+  },
+
+  buttonAdd:{
+    borderWidth: 1,
+    padding: 5,
+    paddingLeft: 10,
+    paddingRight:10,
+    backgroundColor: '#168FFF' 
+  },
+
+  textButton:{
+    fontWeight: 'bold',
+    color: '#FFF'
   }
 })
